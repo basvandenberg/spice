25cc387e2ddd1cf80aa27c0f0a24653027a9c95b
Bastiaan van den Berg <b.a.vandenberg@gmail.com>
2013-04-14 16:30:15 +0200

    pep8

--- spica/job_runner/job_runner.py
+++ spica/job_runner/job_runner.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python
 
-import os, sys, time, glob, shutil, subprocess
-import datetime
+import os
+import sys
+import time
+import glob
+import shutil
+import subprocess
 from daemon import Daemon
 from operator import itemgetter
 
 # define project directory location
 # TODO add a set_root_dir function, or pass spica as parameter...
-project_dir = '/home/bastiaan/Development/spicaweb_test/projects'
-    
+project_dir = '/home/bastiaan/Develop/spicaweb_test/projects'
+
 # define log output files
-log_dir = '/home/bastiaan/Development/spicaweb/job_runner_0.1/job_runner'
+log_dir = '/home/bastiaan/Develop/spica/spica/job_runner'
 pid_f = os.path.join(log_dir, 'daemon.pid')
 
 # pause between checking for jobs in queue
 sleep_interval = 5
+
 
 class JobRunner(Daemon):
 
@@ -35,7 +40,7 @@ class JobRunner(Daemon):
                         stdout_f = fin.readline().strip()
                         stderr_f = fin.readline().strip()
                     jobs.append((project_d, job_f, cmd, timestamp))
-                    
+
             # if found, run the one that was submitted first
             if(len(jobs) > 0):
 
@@ -52,7 +57,7 @@ class JobRunner(Daemon):
 
                 # move job file to running dir
                 shutil.move(wait_f, run_f)
-                
+
                 # redirect standard file descriptors
                 sys.stdout.flush()
                 sys.stderr.flush()
@@ -62,10 +67,10 @@ class JobRunner(Daemon):
                 se = file(self.stderr, 'a+', 0)
                 os.dup2(so.fileno(), sys.stdout.fileno())
                 os.dup2(se.fileno(), sys.stderr.fileno())
-    
+
                 # run the job (in a later stage thread the job...? Popen)
                 retcode = subprocess.call(cmd.split())
-                
+
                 # move running to either done or error based on return code
                 if(retcode == 0):
                     shutil.move(run_f, done_f)
